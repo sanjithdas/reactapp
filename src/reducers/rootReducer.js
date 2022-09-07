18aac84016fa98f899ffceec60ba1194b84f435f
sanjithdas <sanjith.das@gmail.com>
2022-09-07 21:57:34 +1000

    Update rootReducer.js

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,7 +3,7 @@
  * @email [sanjith.das@gmail.com]
  * @create date 2020-11-06 14:45:13
  * @modify date 2021-03-06 22:56:18
- * @desc [Root Reducer - combine room, user, UI reducers]
+ * @desc [Root Reducer - combine task, user, UI reducers]
  */
 // redux import
 import { combineReducers } from "redux";
